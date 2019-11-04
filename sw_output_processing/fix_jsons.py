d247361fda9507d799a75aed768cf5e759ef1b5f
John <urbine.5@buckeyemail.osu.edu>
2019-11-04 14:44:05 -0500

    updates stats processing

--- sw_output_processing/fix_jsons.py
+++ sw_output_processing/fix_jsons.py
@@ -35,39 +35,3 @@ def make_cloud(bank_name, bank_text):
 
 
 def bank_details():
-    jsons_directory = 'jsons'
-    json_files = os.listdir(jsons_directory)
-
-    bank_objects = []
-    for file in json_files:
-        file_path = os.path.join(jsons_directory, file)
-        with open(file_path, 'r') as fin:
-            file_string = fin.read()
-            bank_parts = file_string.split(',')
-
-            bank_name = bank_parts[0].split(':')[1].replace('"', '').strip()
-            bank_text = bank_parts[1].split(':')[1].replace('"', '').replace('\\n', '. ').replace('..', '.').strip()
-            bank_text_fixed = ' '.join(bank_text.split()).strip().replace(' . ', '')
-            bank_num_pages = bank_parts[2].split(':')[1].replace('}', '').replace('"', '').strip()
-
-            bank_object = {
-                "bank_details" : {
-                    "name" : bank_name,
-                    "complete_text" : bank_text_fixed,
-                    "num_pages" : bank_num_pages,
-                    "average_sentence_length": 0,
-                    "bog_index": 0,
-                    "number_stylewriter_chunks": 1
-                }
-            }
-
-            #if len(bank_text_fixed) > 10:
-            #    make_cloud(bank_name, bank_text_fixed)
-            
-            new_bank = Bank(bank_name, bank_object)
-
-            bank_objects.append(new_bank)
-
-    return bank_objects
-
-    
--- sw_output_processing/stats_run.py
+++ sw_output_processing/stats_run.py
@@ -1,26 +1,38 @@
 import os
 import csv
 import json
-from fix_jsons import bank_details
 from time import sleep
+from wordcloud import WordCloud, STOPWORDS 
+import matplotlib.pyplot as plt 
+import pandas as pd 
+
+class ShadowBank:
+    def __init__(self, info):
+        self.data = {
+            "bank_details": {
+                "name": info["name"],
+                "num_pages": info["num_pages"],
+                "master_text": {
+                    "raw_text": info["master_string"],
+                    "bog_index": 0,
+                    "total_words": 0,
+                    "avg_sentence_length": 0,
+                    "number_sw_chunks": 0
+                },
+                "refined_text": {
+                    "raw_text": info["refined_master_string"],
+                    "bog_index": 0,
+                    "total_words": 0,
+                    "avg_sentence_length": 0,
+                    "number_sw_chunks": 0
+                }
+
+            }
+        }
 
 
-class ChunkedBank():
-    def __init__(self, name, total_words, avg_sentence_length, bog_index):
-        self.name = name
-        self.total_words = float(total_words)
-        self.avg_sentence_length = float(avg_sentence_length)
-        self.bog_index = float(bog_index)
-        self.number_chunks = 1
 
-    def get_details(self):
-        avg_total_words = self.total_words / self.number_chunks
-        avg_avg_sentence_length = self.avg_sentence_length / self.number_chunks
-        avg_bog_index = self.bog_index / self.number_chunks
-        return([self.name.replace('_', ' ').replace('.docx', ''), int(avg_total_words) , int(avg_avg_sentence_length), int(avg_bog_index), self.number_chunks])
-    
-
-def get_multi_chunk():
+def process_sw4stats_file():
     file_name = 'sw4stats.txt'
     with open(file_name, 'r') as fin:
         file_contents = fin.readlines()
@@ -30,7 +42,7 @@ def get_multi_chunk():
         if i == 0 or line == '':
             pass
         else:
-            split_line = line.split(':')[1].strip().split()[1:-5]
+            split_line = line.replace('__', '_').split(':')[1].strip().split()[1:-5]
             del split_line[1]
             if 'Clipboard' in split_line:
                 pass
@@ -45,68 +57,11 @@ def get_multi_chunk():
                 desired_bank_data = bank_file_name + ' ' + bank_total_words + ' ' + bank_average_sentence_length + ' ' + bank_bog_index
                 bank_details.append(desired_bank_data.strip())
     
-    bank_details = list(set(bank_details))
-    chunked_banks = []
     for item in bank_details:
-        item_components = item.split()
-        if (item_components[0][0].isdigit() and item_components[0][1] == '_') or (item_components[0][0].isdigit() and item_components[0][1].isdigit() and item_components[0][2] == '_'):
-            chunked_banks.append(item_components)
-
-    chunked_instances = []
-    seen_chunks = []
-    for item in chunked_banks:
-        bank_label = item[0].split('_', 1)
-        bank_name = bank_label[1]
-
-        if bank_name in seen_chunks:
-            for instance in chunked_instances:
-                if instance.name == bank_name:
-                    instance.total_words += float(item[1])
-                    instance.avg_sentence_length += float(item[2])
-                    instance.bog_index += float(item[3])
-                    instance.number_chunks += 1
-
-        else:
-            new_chunked_bank = ChunkedBank(bank_name, item[1], item[2], item[3])
-            chunked_instances.append(new_chunked_bank)
-            seen_chunks.append(bank_name)
+        print(item)
+    
 
     
-    
-    return chunked_instances
-    
-def process_stylewriter_output():
-    banks = bank_details()
-    
-    file_name = 'sw4stats.txt'
-    with open(file_name, 'r') as fin:
-        file_contents = fin.readlines()
-
-    for bank in banks:
-        cur_bank = bank
-
-        for i, line in enumerate(file_contents):
-            if i == 0:
-                pass
-            else:
-                if 'Clipboard' in line:
-                    pass
-                else:
-                    sentence = line.split(':', 1)
-                    remove_colon = ' '.join(sentence)
-                    sentence_parts = remove_colon.split()
-                    
-                    if sentence_parts[5].replace('_', ' ').replace('.docx', '') == bank.name:
-                        cur_bank.total_words = float(sentence_parts[7])
-                        cur_bank.average_sentence_length = float(sentence_parts[8])
-                        cur_bank.bog_index = float(sentence_parts[11])
-                        cur_bank.data["bank_details"]["average_sentence_length"] = float(sentence_parts[8])
-                        cur_bank.data["bank_details"]["bog_index"] = float(sentence_parts[11])
-                    
-                    else:
-                        pass
-    
-    return banks
 
 
 def output_to_csv(banks, chunked_instances):    
@@ -132,9 +87,23 @@ def output_to_csv(banks, chunked_instances):
 
             writer.writerow(out_string)
 def main():
-    chunked_banks = get_multi_chunk()
-    banks = process_stylewriter_output()
-    output_to_csv(banks, chunked_banks)
+    json_directory = 'bank_jsons'
+    json_files = os.listdir(json_directory)
+    for json_file in json_files:
+        try:
+            json_filepath = os.path.join(json_directory, json_file)
+            with open(json_filepath, 'r') as fin:
+                data = json.load(fin)
+        
+        except Exception as e:
+            print(e)
+            print(json_file)
+
+        
+
+
+
+    #process_sw4stats_file()
 
 
 main()
